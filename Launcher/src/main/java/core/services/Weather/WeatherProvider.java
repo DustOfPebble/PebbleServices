2d6a9c7b82ea37136f4112542d45770cfd60b893
Xavier Jaffrezic <xavier@jaffrezic.net>
2017-04-25 22:00:35 +0200

    code cleanup about unused import

--- Launcher/src/main/java/core/services/Weather/WeatherProvider.java
+++ Launcher/src/main/java/core/services/Weather/WeatherProvider.java
@@ -1,9 +1,6 @@
 package core.services.Weather;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
