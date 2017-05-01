9b657fd1452d0dcb08b788c4a5ea1c9dea25d460
Xavier Jaffrezic <xavier@jaffrezic.net>
2017-05-01 15:54:35 +0200

    reindexing weather ...

--- Launcher/src/main/java/core/services/Weather/WeatherCode.java
+++ Launcher/src/main/java/core/services/Weather/WeatherCode.java
@@ -7,29 +7,29 @@ package core.services.Weather;
 public class WeatherCode {
 /*********************************************************
  * Includes from Smartwatch application
- * #define Sunny 0
- * #define SunnyCloudy 1
- * #define Cloudy 2
- * #define Rainy 3
- * #define Stormy 4
- * #define SunnyRainy 5
- * #define Snowy 6
+ * #define Sunny 1
+ * #define SunnyCloudy 2
+ * #define Cloudy 3
+ * #define Rainy 4
+ * #define Stormy 5
+ * #define SunnyRainy 6
+ * #define Snowy 7
  * *********************************************************/
 
     static public final String Sunny = "01";
-    static public final int SunnyID = 0;
+    static public final int SunnyID = 1;
     static public final String SunnyCloudy = "02";
-    static public final int SunnyCloudyID = 1;
+    static public final int SunnyCloudyID = 2;
     static public final String Cloudy = "03";
-    static public final int CloudyID = 2;
+    static public final int CloudyID = 3;
     static public final String HeavyCloudy = "04";
-    static public final int HeavyCloudyID = 2;
+    static public final int HeavyCloudyID = 3;
     static public final String Rainy = "09";
-    static public final int RainyID = 3;
-    static public final String SunnyRainy = "10";
-    static public final int SunnyRainyID = 5;
+    static public final int RainyID = 4;
     static public final String Stormy = "11";
-    static public final int StormyID = 4;
+    static public final int StormyID = 5;
+    static public final String SunnyRainy = "10";
+    static public final int SunnyRainyID = 6;
     static public final String Snowy = "12";
-    static public final int SnowyID = 6;
+    static public final int SnowyID = 7;
 }
